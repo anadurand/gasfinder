46690d5628283a9c1c6ce0eede0d9e47adf0e06a
anadurand <ana.dc1004@gmail.com>
2017-06-06 23:11:32 -0500

    añadiendo el mapa

--- js/components/gmap.js
+++ js/components/gmap.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const map = new GMaps({
-  div: '#map',
-  lat: -12.043333,
-  lng: -77.028333
-});
 
 // GMaps.geolocate({
 //   success: function(position) {
@@ -19,6 +14,41 @@ const map = new GMaps({
 //
 // });
 
+const ShowMap = (station) =>{
+  const map = new GMaps({
+    div: '#map',
+    lat: -12.043333,
+    lng: -77.028333
+  })
+  // var myLatLng = {lat: -12.043333, lng: -77.028333};
+
+  GMaps.geolocate({
+  success: function(position) {
+    map.setCenter(position.coords.latitude, position.coords.longitude);
+    map.setZoom(13);
+      map.addMarker({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        title: "position actual",
+        click: function(e) {
+          alert('stations');
+        }
+      });
+      map.drawRoute({
+        origin: [position.coords.latitude, position.coords.longitude],
+        destination: [state.selectedStation.lat, state.selectedStation.long],
+        travelMode: 'driving',
+      });
+      map.addMarker({
+        lat: state.selectedStation.lat,
+        lng: state.selectedStation.long,
+        title: state.selectedStation.name +"\n"+ state.selectedStation.address,
+        click: function(e) {
+          alert('stations');
+        }
+      });
+    },
+  });
 
 const Gmaps = (update) => {
   const parent = $('<div></div>');
